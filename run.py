36e6a7e78bddd2bed245fe9fc4dc2e0d20c1e78e
ilya <shabalin_ilya_omu@mail.ru>
2021-11-29 20:42:09 +0700

    Add ffmpeg fileToWav function.

---
+++ run.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+from vosk import Model, KaldiRecognizer
+import wave
+import json
+import time
+import os
+
+WAV_RATE = 16000
+DAEMON_RESPONSE_FREQUENCY = 0.3  # seconds
+
+# global_path = "/var/www/html/apps/audio/data/ru/"
+global_path = ""
+
+wavs_path = global_path + "wavs/"
+text_path = global_path + "texts/"
+
+model = Model("models/ru/")
+rec = KaldiRecognizer(model, WAV_RATE)
+
+
+def dirSystemInit():
+    os.makedirs(wavs_path, exist_ok=True)
+    os.makedirs(text_path, exist_ok=True)
+
+
+def recognizer_daemon():
+    filenames = get_new_files()
+    for filename in filenames:
+        wav_file = fileToWav(filename)
+        text = read_wav(wav_file)
+        write_transcript(wav_file, text)
+        delete_recognized_wav(wav_file)
+
+
+def fileToWav(filename):
+    input_file = filename
+    output_file = os.path.splitext(filename)[0] + ".wav"
+    # using ffmpeg app for convert all audio file for .wav with rate=16000 and mono
+    ffmpeg_command = "ffmpeg -hide_banner -loglevel error -i {input_file} -y -ac 1 -ar {sample_rate} {output_file}"\
+        .format(input_file=wavs_path + input_file, sample_rate=WAV_RATE, output_file=wavs_path + output_file)
+    os.system(ffmpeg_command)
+    os.remove(wavs_path + input_file)
+    return output_file
+
+
+def get_new_files():
+    files = os.listdir(wavs_path)
+    return files
+
+
+def read_wav(filename):
+    with open(wavs_path + filename, "rb") as wf:
+        wf.read(44)  # skip wav header
+        recognized_words = []
+
+        while True:
+            data = wf.read(4000)
+            if len(data) == 0:
+                break
+            if rec.AcceptWaveform(data):
+                res = json.loads(rec.Result())
+                recognized_words.append(res['text'])
+
+        res = json.loads(rec.FinalResult())
+        recognized_words.append(res['text'])
+        text = ' '.join(recognized_words)
+        return text
+
+
+def write_transcript(filename, text):
+    print(text)
+    with open(text_path + os.path.splitext(filename)[0] + '.txt', 'w') as transcript:
+        transcript.write(text)
+
+
+def delete_recognized_wav(filename):
+    os.remove(wavs_path + filename)
+
+
+if __name__ == '__main__':
+    dirSystemInit()
+    print("The daemon has been successfully launched!")
+    try:
+        while True:
+            recognizer_daemon()
+            time.sleep(DAEMON_RESPONSE_FREQUENCY)
+
+    except FileNotFoundError:
+        print("No files to recognize!")
+        with open(text_path, 'w') as result:
+            result.write('')
+
+    except wave.Error:
+        print(wave.Error)
+
+    finally:
+        print("Daemon was interrupted for some reasons. "
+              "All wavs and texts files will be immediate deleted for this session!")
+#        shutil.rmtree(wavs_path)
+#        shutil.rmtree(text_path)
